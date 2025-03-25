85bdf3f6291c67c229826927a622ff7db2fb4c16
puturangga21 <puturangga21@gmail.com>
2025-03-25 21:12:52 +0800

    fix: cors

--- backend/middleware/authUser.js
+++ backend/middleware/authUser.js
@@ -34,7 +34,7 @@ const authUser = (req, res) => {
       res.cookie("accessToken", accessToken, {
         secure: true,
         httpOnly: true,
-        sameSite: "None", // Helps prevent CSRF attacks
+        sameSite: "none", // Helps prevent CSRF attacks
         expires: new Date(Date.now() + 15 * 60 * 1000),
       });
 
