726f13dc560e71c459a495f960f3e25b5da06a9d
puturangga21 <puturangga21@gmail.com>
2025-04-14 11:05:38 +0800

    frontend: update

--- frontend/src/components/header-sidebar.jsx
+++ frontend/src/components/header-sidebar.jsx
@@ -19,7 +19,7 @@ export function HeaderSidebar() {
             <MapPinned className='size-4' />
           </div>
           <div className='grid flex-1 text-left text-sm leading-tight'>
-            <span className='truncate font-semibold'>GIS KemuMai</span>
+            <span className='truncate font-semibold'>GIS TemuKangen</span>
             <span className='truncate text-xs'>v1.0.0</span>
           </div>
         </SidebarMenuButton>
